26e522b29fdca9363c815f41465ffb17ec64358b
Junho Kim <chordplaylist@gmail.com>
2022-06-21 02:45:38 +0900

    22_06_21

--- src/main/java/com/javaex/controller/UserController.java
+++ src/main/java/com/javaex/controller/UserController.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,77 +15,72 @@ import com.javaex.vo.UserVo;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	
+
 	@Autowired
 	UserService userService;
-	/***************************************로그인***************************************/
+
+	/****************************************로그인***************************************/
 	// 로그인 폼
-	@RequestMapping(value = "/loginform", method= {RequestMethod.GET, RequestMethod.POST})
+	@RequestMapping(value = "/loginform", method = { RequestMethod.GET, RequestMethod.POST })
 	public String loginForm() {
-			
-		
+
 		return "user/loginForm";
 	}
-	
+
 	// 로그인
-	@RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
+	@RequestMapping(value = "/login", method = { RequestMethod.GET, RequestMethod.POST })
 	public String login(@ModelAttribute UserVo userVo, HttpSession session) {
-		
+		System.out.println("UserController > login()");
 		UserVo authUser = userService.login(userVo);
-		
-		if(authUser != null) {
+
+		if (authUser != null) {
 			session.setAttribute("authUser", authUser);
 			System.out.println("login");
 			return "redirect:../main";
-		}else {
+		} else {
 			System.out.println("로그인 실패입니다.");
 			return "redirect:./loginform?result=fail";
 		}
-		
 	}
-	
+
 	// 로그아웃
-	@RequestMapping(value = "/logout", method = {RequestMethod.GET, RequestMethod.POST})
+	@RequestMapping(value = "/logout", method = { RequestMethod.GET, RequestMethod.POST })
 	public String logout(HttpSession session) {
+		System.out.println("UserController > logout()");
 		session.invalidate();
-		
+
 		return "redirect:../main";
 	}
-	
-	
-	/***************************************회원가입***************************************/
-	
-	
+
+	/****************************************회원가입***************************************/
 	// 회원가입 폼
-	@RequestMapping(value = "/joinform", method = {RequestMethod.GET, RequestMethod.POST})
+	@RequestMapping(value = "/joinform", method = { RequestMethod.GET, RequestMethod.POST })
 	public String joinForm() {
 		System.out.println("UserController > joinForm()");
-		
+
 		return "user/joinForm";
 	}
-	
+
 	// 회원가입
-	
-	@RequestMapping(value = "/join", method = {RequestMethod.GET, RequestMethod.POST})
+	@RequestMapping(value = "/join", method = { RequestMethod.GET, RequestMethod.POST })
 	public String join(@ModelAttribute UserVo userVo) {
 		System.out.println("UserController > join()");
-		
+
 		int count = userService.join(userVo);
-		
+
 		System.out.println(count);
-		
+
 		return "redirect:/user/joinok";
 	}
-	
-	@RequestMapping(value = "/joinok", method = {RequestMethod.GET, RequestMethod.POST} )
+
+	// 회원가입 완료
+	@RequestMapping(value = "/joinok", method = { RequestMethod.GET, RequestMethod.POST })
 	public String joinOk() {
 		System.out.println("UserController > loginOk()");
-		
+
 		return "user/joinOk";
 	}
-	
-	
-	
+
 	/***************************************수정***************************************/
 	// 수정 폼
 	@RequestMapping(value = "/modifyform", method = {RequestMethod.GET, RequestMethod.POST})
@@ -96,23 +90,24 @@ public class UserController {
 		HttpSession session = req.getSession();
 		UserVo authUser =  (UserVo) session.getAttribute("authUser");
 		
-		UserVo user = userService.getUser(authUser.getNo());
-		
-		session.setAttribute("authUser", user);
+		UserVo userVo = userService.getUser(authUser.getNo());
+		session.setAttribute("userVo", userVo);
 		
 		return "user/modifyForm";
 	}
 	
-	
-	@RequestMapping(value = "/modify", method = { RequestMethod.GET, RequestMethod.POST})
-	public String modify(@ModelAttribute UserVo authUser, HttpServletRequest req) {
+	// 수정
+	@RequestMapping(value = "/modify", method = { RequestMethod.GET, RequestMethod.POST })
+	public String modify(@ModelAttribute UserVo userVo, HttpServletRequest req) {
+		
 		System.out.println("UserController > modify()");
 		HttpSession session = req.getSession();
 		
-		userService.modify(authUser);
-		session.setAttribute("authUser", authUser);
+		UserVo authUser = (UserVo) session.getAttribute("authUser");
 		
-		
+		userService.modify(userVo);
+		session.setAttribute("authUser", userService.login(userVo));
+
 		return "redirect:../main";
 	}
 
--- webapp/WEB-INF/views/user/modifyForm.jsp
+++ webapp/WEB-INF/views/user/modifyForm.jsp
@@ -57,32 +57,32 @@
 							<!-- 아이디 -->
 							<div class="form-group">
 								<label class="form-text" for="input-uid">아이디</label> 
-								<span class="text-large bold">${authUser.id }</span>
-								<input type="hidden" name ="id" value="${authUser.id}">
+								<span class="text-large bold">${userVo.id }</span>
+								<input type="hidden" name ="id" value="${userVo.id}">
 							</div>
 	
 							<!-- 비밀번호 -->
 							<div class="form-group">
 								<label class="form-text" for="input-pass">패스워드</label> 
-								<input type="text" id="input-pass" name="password" value="${authUser.password }" placeholder="비밀번호를 입력하세요"	>
+								<input type="text" id="input-pass" name="password" value="${userVo.password }" placeholder="비밀번호를 입력하세요"	>
 							</div>
 	
 							<!-- 이메일 -->
 							<div class="form-group">
 								<label class="form-text" for="input-name">이름</label> 
-								<input type="text" id="input-name" name="name" value="${authUser.name }" placeholder="이름을 입력하세요">
+								<input type="text" id="input-name" name="name" value="${userVo.name }" placeholder="이름을 입력하세요">
 							</div>
 	
 							<!-- //나이 -->
 							<div class="form-group">
 								<span class="form-text">성별</span> 
-								<c:if test = "${authUser.gender eq 'male' }">
+								<c:if test = "${userVo.gender eq 'male' }">
 									<label for="rdo-male">남</label> 
 									<input type="radio" id="rdo-male" name="gender" value="male" checked="checked"> 
 									<label for="rdo-female">여</label> 
 									<input type="radio" id="rdo-female" name="gender" value="female" >
 								</c:if>
-								<c:if test = "${authUser.gender eq 'female' }">
+								<c:if test = "${userVo.gender eq 'female' }">
 									<label for="rdo-male">남</label> 
 									<input type="radio" id="rdo-male" name="gender" value="male" > 
 									<label for="rdo-female">여</label> 
